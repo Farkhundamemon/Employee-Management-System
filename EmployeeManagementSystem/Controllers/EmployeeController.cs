e7b37832c2668a99209546846a29a2c2f8638013
test name <test@email.com>
2026-08-01 22:30:45 +0500

    Added Employee List feature with Controller and View

---
+++ EmployeeManagementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeManagementSystem.Models;
+using System.Linq;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
