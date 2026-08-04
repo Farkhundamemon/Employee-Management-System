e23f13bf9def07222a00643fb422827dfb96286b
test name <test@email.com>
2026-08-04 12:30:14 +0500

    Completed Leave Management module with full CRUD

---
+++ EmployeeManagementSystem/Controllers/LeaveController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeManagementSystem.Models;
+using System.Linq;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class LeaveController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public LeaveController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var leaves = _context.Leaves.ToList();
+            return View(leaves);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Leave leave)
+        {
+            _context.Leaves.Add(leave);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var leave = _context.Leaves.Find(id);
+            return View(leave);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Leave leave)
+        {
+            _context.Leaves.Update(leave);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var leave = _context.Leaves.Find(id);
+            if (leave != null)
+            {
+                _context.Leaves.Remove(leave);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
