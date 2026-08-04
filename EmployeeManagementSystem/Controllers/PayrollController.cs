1e4772a272bd6d2641566b36cbff9765220de2cc
test name <test@email.com>
2026-08-04 12:39:55 +0500

    Completed Payroll module - all 5 core modules now complete

---
+++ EmployeeManagementSystem/Controllers/PayrollController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeManagementSystem.Models;
+using System.Linq;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class PayrollController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PayrollController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var payrolls = _context.Payrolls.ToList();
+            return View(payrolls);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Payroll payroll)
+        {
+            payroll.net_salary = payroll.basic_salary - payroll.deduction;
+            _context.Payrolls.Add(payroll);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var payroll = _context.Payrolls.Find(id);
+            return View(payroll);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Payroll payroll)
+        {
+            payroll.net_salary = payroll.basic_salary - payroll.deduction;
+            _context.Payrolls.Update(payroll);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var payroll = _context.Payrolls.Find(id);
+            if (payroll != null)
+            {
+                _context.Payrolls.Remove(payroll);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
